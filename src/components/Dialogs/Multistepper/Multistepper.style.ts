c1695196e5dab3161b4a97ddd21e960e780b8cc2
drillprop <drylbartosz@gmail.com>
2021-05-26 08:56:45 +0200

    multistepper steps improvement

--- src/components/Dialogs/Multistepper/Multistepper.style.ts
+++ src/components/Dialogs/Multistepper/Multistepper.style.ts
@@ -46,7 +46,9 @@ export const StyledStepsInfoContainer = styled.div`
   display: grid;
 
   ${media.small} {
-    grid-template-columns: 1fr 1fr 1fr;
+    /* 5 = 3 steps + 2 visible chevrons  */
+    grid-template-columns: repeat(5, auto);
+    align-items: center;
     grid-column-gap: ${sizes(4)};
   }
 `
